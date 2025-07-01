9ac3cdecdcc4143eb7959b20eed0c902fba00798
Muhammad bin Javed <muhammad.javed@movexm.com>
2025-07-01 20:00:28 +0500

    Deleted extra files

---
+++ frontend/remote-app/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Remote App</h1>
+    </div>
+  );
+};
+
+export default Home;
